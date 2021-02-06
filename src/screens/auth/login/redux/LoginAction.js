ca7cef2e0ed9a12445324114519f3722e10e175d
kanerodgers7 <jellyfish9629@gmail.com>
2021-02-06 17:09:10 +0530

    Make login action async

--- src/screens/auth/login/redux/LoginAction.js
+++ src/screens/auth/login/redux/LoginAction.js
@@ -1,29 +1,25 @@
+import { LOGIN_REDUX_CONSTANTS } from './LoginReduxConstants';
 import AuthApiService from '../../services/AuthApiService';
 import { errorNotification } from '../../../../common/Toast';
-import { LOGIN_REDUX_CONSTANTS } from './LoginReduxConstants';
 
 export const loginUser = (email, password) => {
-  return dispatch => {
-    const data = {
-      email,
-      password,
-    };
+  return async dispatch => {
+    try {
+      const data = { userId: email, password };
+      const response = await AuthApiService.loginUser(data);
 
-    AuthApiService.loginUser(data)
-      .then(response => {
-        if (response.data.status === 'SUCCESS') {
-          dispatch({
-            type: LOGIN_REDUX_CONSTANTS.LOGIN_USER_ACTION,
-            data: response.data.data,
-          });
-        }
-      })
-      .catch(e => {
-        if (e.response.data.status === undefined) {
-          errorNotification('It seems like server is down, Please try again later.');
-        } else if (e.response.data.status === 'INTERNAL_SERVER_ERROR') {
-          errorNotification('Internal server error');
-        }
-      });
+      if (response.data.status === 'SUCCESS') {
+        dispatch({
+          type: LOGIN_REDUX_CONSTANTS.LOGIN_USER_ACTION,
+          data: response.data.data,
+        });
+      }
+    } catch (e) {
+      if (e.response.data.status === undefined) {
+        errorNotification('It seems like server is down, Please try again later.');
+      } else if (e.response.data.status === 'INTERNAL_SERVER_ERROR') {
+        errorNotification('Internal server error');
+      }
+    }
   };
 };
