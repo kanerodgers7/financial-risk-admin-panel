a5ea3bee17bc52de66d25a7503c65014f4119f2f
kanerodgers7 <jellyfish9629@gmail.com>
2021-02-06 20:14:47 +0530

    Added reset password url

--- src/constants/UrlConstants.js
+++ src/constants/UrlConstants.js
@@ -1,7 +1,8 @@
 export const BASE_URL = 'https://client.trad.dev.gradlesol.com/app/';
 
 export const AUTH_URLS = {
-  LOGIN_URL: `${BASE_URL}rp/auth/login`,
-  FORGOT_PASSWORD_URL: `${BASE_URL}rp/auth/forget-password`,
-  VERIFY_OTP_URL: `${BASE_URL}rp/auth/verify-otp`,
+  LOGIN_URL: `${BASE_URL}rp/auth/login/`,
+  FORGOT_PASSWORD_URL: `${BASE_URL}rp/auth/forget-password/`,
+  VERIFY_OTP_URL: `${BASE_URL}rp/auth/verify-otp/`,
+  RESET_PASSWORD_URL: `${BASE_URL}rp/auth/`,
 };
