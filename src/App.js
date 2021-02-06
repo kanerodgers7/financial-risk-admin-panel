e2761638bdb0e06db9775ae5d90a2dde43ba1a32
kanerodgers7 <jellyfish9629@gmail.com>
2021-02-06 13:47:44 +0530

    Notification component fix

--- src/App.js
+++ src/App.js
@@ -9,18 +9,17 @@ import { store } from './redux/store';
 function App() {
   return (
     <Provider store={store}>
-      <Notifications>
-        <Router>
-          <Switch>
-            <Route exact path="/">
-              <Redirect to="/login" />
-            </Route>
-            <Route exact path="/login" component={LoginScreen} />
-            <Route exact path="/login/:screenType" component={ForgotPassword} />
-            <Route exact path="/dashboard" component={Dashboard} />
-          </Switch>
-        </Router>
-      </Notifications>
+      <Notifications />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/login" />
+          </Route>
+          <Route exact path="/login" component={LoginScreen} />
+          <Route exact path="/login/:screenType" component={ForgotPassword} />
+          <Route exact path="/dashboard" component={Dashboard} />
+        </Switch>
+      </Router>
     </Provider>
   );
 }
