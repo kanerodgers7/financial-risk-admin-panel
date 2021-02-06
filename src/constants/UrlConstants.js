9676287c47c3cfee338b4501b24b26f38285f447
kanerodgers7 <jellyfish9629@gmail.com>
2021-02-06 19:55:41 +0530

    Added verify url constant

--- src/constants/UrlConstants.js
+++ src/constants/UrlConstants.js
@@ -3,4 +3,5 @@ export const BASE_URL = 'https://client.trad.dev.gradlesol.com/app/';
 export const AUTH_URLS = {
   LOGIN_URL: `${BASE_URL}rp/auth/login`,
   FORGOT_PASSWORD_URL: `${BASE_URL}rp/auth/forget-password`,
+  VERIFY_OTP_URL: `${BASE_URL}rp/auth/verify-otp`,
 };
