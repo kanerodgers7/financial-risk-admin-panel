84612c88129c0ec8d7ce6b2e6c9e1684b0bf3b3e
kanerodgers7 <jellyfish9629@gmail.com>
2021-02-06 19:59:21 +0530

    Bind forgot password apis to screen

--- src/screens/auth/forgotPassword/ForgotPassword.js
+++ src/screens/auth/forgotPassword/ForgotPassword.js
@@ -1,100 +1,55 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
 import './ForgotPassword.scss';
+import { useState } from 'react/cjs/react.production.min';
+import { Link, useHistory } from 'react-router-dom';
 import AuthScreenContainer from '../common/CommonAuthScreen/AuthScreenContainer';
 import Button from '../../../common/Button/Button';
 import BigInput from '../../../common/BigInput/BigInput';
+import { errorNotification } from '../../../common/Toast';
+import { checkForEmail, replaceHiddenCharacters } from '../../../helpers/ValidationHelper';
+import { forgotPassword } from './redux/ForgotPasswordAction';
 
 function ForgotPassword() {
-  const { screenType } = useParams();
+  const [email, setEmail] = useState();
+
+  const history = useHistory();
+
+  const onChangeEmail = e => {
+    const emailText = e.target.value;
+
+    setEmail(emailText);
+  };
+
+  const onClickForgotPassword = async () => {
+    if (email.toString().trim().length === 0) errorNotification('Please enter email');
+    else if (!checkForEmail(replaceHiddenCharacters(email)))
+      errorNotification('Please enter a valid email');
+    else {
+      try {
+        await forgotPassword(email.trim());
+        history.push('/verify-otp');
+      } catch (e) {
+        /**/
+      }
+    }
+  };
 
   return (
     <AuthScreenContainer>
-      {screenType === 'forgot' ? (
-        <>
-          <div className="login-field-name">Email or Number</div>
-          <BigInput
-            prefix="drafts"
-            prefixClass="login-input-icon"
-            type="email"
-            placeholder="Enter email or number"
-          />
-          <div className="login-action-row">
-            <div />
-            <a href="/">Back To Login</a>
-          </div>
-          <Button title="SendOTP" buttonType="primary" />
-        </>
-      ) : (
-        ''
-      )}
-
-      {screenType === 'otp' ? (
-        <>
-          <div className="login-field-name">Email or Number</div>
-          <BigInput
-            prefix="drafts"
-            prefixClass="login-input-icon"
-            type="email"
-            placeholder="Enter email or number"
-          />
-          <div className="login-field-name">Enter OTP</div>
-          <div className="otp-row">
-            <div className="login-input">
-              <input maxLength="1" placeholder="0" />
-            </div>
-            <div className="login-input">
-              <input maxLength="1" placeholder="0" />
-            </div>
-            <div className="login-input">
-              <input maxLength="1" placeholder="0" />
-            </div>
-            <div className="login-input">
-              <input maxLength="1" placeholder="0" />
-            </div>
-            <div className="login-input">
-              <input maxLength="1" placeholder="0" />
-            </div>
-            <div className="login-input">
-              <input maxLength="1" placeholder="0" />
-            </div>
-          </div>
-          <div className="login-action-row">
-            <div />
-            <a href="/">Back To Login</a>
-          </div>
-          <Button title="Resend OTP" buttonType="outlined-secondary" />
-          <Button title="Submit" buttonType="secondary" className="ml-15" />
-        </>
-      ) : (
-        ''
-      )}
-
-      {screenType === 'reset' ? (
-        <>
-          <div className="login-field-name">New Password</div>
-          <BigInput
-            prefix="lock_open"
-            prefixClass="login-input-icon"
-            type="password"
-            placeholder="Enter New password"
-          />
-          <div className="login-field-name">Re-enter Password</div>
-          <BigInput
-            prefix="lock_open"
-            prefixClass="login-input-icon"
-            type="password"
-            placeholder="Re Enter password"
-          />
-          <div className="login-action-row">
-            <div />
-            <a href="/">Back To Login</a>
-          </div>
-          <Button title="Set New Password" buttonType="secondary" className="ml-15" />
-        </>
-      ) : (
-        ''
-      )}
+      <div className="login-field-name">Email or Number</div>
+      <BigInput
+        prefix="drafts"
+        prefixClass="login-input-icon"
+        type="email"
+        placeholder="Enter email or number"
+        value={email}
+        onChange={onChangeEmail}
+      />
+      <div className="login-action-row">
+        <div />
+        <Link to="/login">Back To Login</Link>
+      </div>
+      <Button title="SendOTP" buttonType="primary" onClick={onClickForgotPassword} />
     </AuthScreenContainer>
   );
 }
