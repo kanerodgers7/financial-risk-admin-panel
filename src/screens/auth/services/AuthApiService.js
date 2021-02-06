332a2ae97cfee3b32a89fe6d3576a472d540a798
kanerodgers7 <jellyfish9629@gmail.com>
2021-02-06 20:01:53 +0530

    Added verify otp service

--- src/screens/auth/services/AuthApiService.js
+++ src/screens/auth/services/AuthApiService.js
@@ -3,7 +3,8 @@ import { AUTH_URLS } from '../../../constants/UrlConstants';
 
 const AuthApiService = {
   loginUser: data => ApiService.postData(AUTH_URLS.LOGIN_URL, data),
-  forgotPassword: data => ApiService.postData(AUTH_URLS.LOGIN_URL, data),
+  forgotPassword: data => ApiService.postData(AUTH_URLS.FORGOT_PASSWORD_URL, data),
+  verifyOtp: data => ApiService.postData(AUTH_URLS.VERIFY_OTP_URL, data),
 };
 
 export default AuthApiService;
