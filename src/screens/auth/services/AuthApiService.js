55e5c913db289a4adbff5e9fbbc6203dfff78ff9
kanerodgers7 <jellyfish9629@gmail.com>
2021-02-06 20:15:04 +0530

    Added reset password api

--- src/screens/auth/services/AuthApiService.js
+++ src/screens/auth/services/AuthApiService.js
@@ -1,10 +1,16 @@
 import ApiService from '../../../services/api-service/ApiService';
 import { AUTH_URLS } from '../../../constants/UrlConstants';
+import { SESSION_VARIABLES } from '../../../constants/SessionStorage';
 
 const AuthApiService = {
   loginUser: data => ApiService.postData(AUTH_URLS.LOGIN_URL, data),
   forgotPassword: data => ApiService.postData(AUTH_URLS.FORGOT_PASSWORD_URL, data),
   verifyOtp: data => ApiService.postData(AUTH_URLS.VERIFY_OTP_URL, data),
+  resetPassword: data =>
+    ApiService.postData(
+      `${AUTH_URLS.RESET_PASSWORD_URL + SESSION_VARIABLES.USER_ID}/reset-password`,
+      data
+    ),
 };
 
 export default AuthApiService;
