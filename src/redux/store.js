9a6b4c639ffcebda767b3e27c0f2e1c98362032c
kanerodgers7 <jellyfish9629@gmail.com>
2021-02-06 13:09:56 +0530

    Moved root reducer to top level

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import rootReducer from './reducers/RootReducer';
+import rootReducer from './RootReducer';
 
 export const store = createStore(
   rootReducer,
