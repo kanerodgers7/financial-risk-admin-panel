c04dd1eb640fe2a5192b09bbeaa17dd7198d06d1
kanerodgers7 <jellyfish9629@gmail.com>
2021-02-06 18:08:59 +0530

    Added session methods for local storage

--- src/helpers/LocalStorageHelper.js
+++ src/helpers/LocalStorageHelper.js
@@ -1,13 +1,31 @@
+import { SESSION_STORAGE } from '../constants/SessionStorage';
+
 export const AUTH_TOKEN = 'userToken';
 
-export const saveAuthTokenLocalStorage = ({ authToken }) => {
+export const saveAuthTokenLocalStorage = authToken => {
+  SESSION_STORAGE.USER_TOKEN = authToken;
   localStorage.setItem(AUTH_TOKEN, authToken);
 };
 
+export const saveTokenToSession = authToken => {
+  SESSION_STORAGE.USER_TOKEN = authToken;
+};
+
+export const saveTokenFromLocalStorageToSession = () => {
+  const authToken = localStorage.getItem(AUTH_TOKEN);
+  if (authToken) {
+    SESSION_STORAGE.USER_TOKEN = authToken;
+  }
+};
+
 export const getAuthTokenLocalStorage = () => {
+  if (SESSION_STORAGE.USER_TOKEN) {
+    return SESSION_STORAGE.USER_TOKEN;
+  }
   return localStorage.getItem(AUTH_TOKEN);
 };
 
 export const clearAuthToken = () => {
+  SESSION_STORAGE.USER_TOKEN = null;
   localStorage.removeItem(AUTH_TOKEN);
 };
