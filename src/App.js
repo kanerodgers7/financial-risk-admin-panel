03a7ff8729bd300bc650b674d7fc6783ea57bedc
kanerodgers7 <jellyfish9629@gmail.com>
2021-02-06 13:30:02 +0530

    Added toast messages in app js

--- src/App.js
+++ src/App.js
@@ -3,21 +3,24 @@ import LoginScreen from "./screens/auth/login/LoginScreen";
 import ForgotPassword from "./screens/auth/ForgotPassword/ForgotPassword";
 import Dashboard from "./common/Dashboard/Dashboard";
 import {Provider} from "react-redux";
+import Notifications from 'react-notify-toast';
 import store from "./redux/store";
 
 function App() {
     return (
             <Provider store={store}>
-                <Router>
-                    <Switch>
-                        <Route exact path="/">
-                            <Redirect to="/login"/>
-                        </Route>
-                        <Route exact path="/login" component={LoginScreen}/>
-                        <Route exact path="/login/:screenType" component={ForgotPassword}/>
-                        <Route exact path="/dashboard" component={Dashboard}/>
-                    </Switch>
-                </Router>
+                <Notifications>
+                    <Router>
+                        <Switch>
+                            <Route exact path="/">
+                                <Redirect to="/login"/>
+                            </Route>
+                            <Route exact path="/login" component={LoginScreen}/>
+                            <Route exact path="/login/:screenType" component={ForgotPassword}/>
+                            <Route exact path="/dashboard" component={Dashboard}/>
+                        </Switch>
+                    </Router>
+                </Notifications>
             </Provider>
     );
 }
