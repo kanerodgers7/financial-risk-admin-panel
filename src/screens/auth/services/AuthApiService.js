335f9f1ca9fa4f49838ea5f08feab915e73a6dff
kanerodgers7 <jellyfish9629@gmail.com>
2021-02-06 17:08:14 +0530

    Changed method for login user api

--- src/screens/auth/services/AuthApiService.js
+++ src/screens/auth/services/AuthApiService.js
@@ -2,7 +2,7 @@ import ApiService from '../../../services/api-service/ApiService';
 import { AUTH_URLS } from '../../../constants/UrlConstants';
 
 const AuthApiService = {
-  loginUser: data => ApiService.putData(AUTH_URLS.LOGIN_URL, data),
+  loginUser: data => ApiService.postData(AUTH_URLS.LOGIN_URL, data),
 };
 
 export default AuthApiService;
