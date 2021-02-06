c1e0d9516e394378f26cdd0a967c2a9108ee0d48
kanerodgers7 <jellyfish9629@gmail.com>
2021-02-06 18:11:24 +0530

    Save token to session from local storage

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,18 +1,36 @@
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+import { useEffect } from 'react/cjs/react.production.min';
 import LoginScreen from '../screens/auth/login/LoginScreen';
 import ForgotPassword from '../screens/auth/forgotPassword/ForgotPassword';
 import Dashboard from '../common/Dashboard/Dashboard';
+import { saveTokenFromLocalStorageToSession } from '../helpers/LocalStorageHelper';
 
 function Routes() {
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
+  //
+  useEffect(() => {
+    saveTokenFromLocalStorageToSession();
+    //   if (SESSION_STORAGE.USER_TOKEN) {
+    //     setIsLoggedIn(true);
+    //   }
+  }, []);
+
   return (
     <Router>
       <Switch>
+        {/* {isLoggedIn ? ( */}
+        {/*  <> */}
+        {/*  </> */}
+        {/* ) : ( */}
+        {/*  <> */}
+        <Route exact path="/dashboard" component={Dashboard} />
         <Route exact path="/">
           <Redirect to="/login" />
         </Route>
         <Route exact path="/login" component={LoginScreen} />
         <Route exact path="/forgot-password/:screenType" component={ForgotPassword} />
-        <Route exact path="/dashboard" component={Dashboard} />
+        {/* </> */}
+        {/* )} */}
       </Switch>
     </Router>
   );
