38a73727b64618054821135c3666ac702d8db9bc
kanerodgers7 <jellyfish9629@gmail.com>
2021-02-06 13:06:25 +0530

    Setup redux in app js

--- src/App.js
+++ src/App.js
@@ -2,22 +2,24 @@ import {BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom
 import LoginScreen from "./screens/auth/login/LoginScreen";
 import ForgotPassword from "./screens/auth/ForgotPassword/ForgotPassword";
 import Dashboard from "./common/Dashboard/Dashboard";
+import {Provider} from "react-redux";
+import store from "./redux/store";
 
 function App() {
-  return (
-    <div>
-      <Router>
-        <Switch>
-            <Route exact path="/" >
-                <Redirect to="/login" />
-            </Route>
-          <Route exact path="/login" component={LoginScreen}/>
-          <Route exact path="/login/:screenType" component={ForgotPassword}/>
-          <Route exact path="/dashboard" component={Dashboard}/>
-        </Switch>
-      </Router>
-    </div>
-  );
+    return (
+            <Provider store={store}>
+                <Router>
+                    <Switch>
+                        <Route exact path="/">
+                            <Redirect to="/login"/>
+                        </Route>
+                        <Route exact path="/login" component={LoginScreen}/>
+                        <Route exact path="/login/:screenType" component={ForgotPassword}/>
+                        <Route exact path="/dashboard" component={Dashboard}/>
+                    </Switch>
+                </Router>
+            </Provider>
+    );
 }
 
 export default App;
