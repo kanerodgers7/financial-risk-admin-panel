11c5d28378f114eb3cea8df9c068f2813e5b2657
kanerodgers7 <jellyfish9629@gmail.com>
2021-02-08 20:10:19 +0530

    Added resend otp api service

--- src/screens/auth/services/AuthApiService.js
+++ src/screens/auth/services/AuthApiService.js
@@ -1,16 +1,12 @@
 import ApiService from '../../../services/api-service/ApiService';
 import { AUTH_URLS } from '../../../constants/UrlConstants';
-import { SESSION_VARIABLES } from '../../../constants/SessionStorage';
 
 const AuthApiService = {
   loginUser: data => ApiService.postData(AUTH_URLS.LOGIN_URL, data),
   forgotPassword: data => ApiService.postData(AUTH_URLS.FORGOT_PASSWORD_URL, data),
   verifyOtp: data => ApiService.postData(AUTH_URLS.VERIFY_OTP_URL, data),
-  resetPassword: data =>
-    ApiService.postData(
-      `${AUTH_URLS.RESET_PASSWORD_URL + SESSION_VARIABLES.USER_ID}/reset-password`,
-      data
-    ),
+  resentOtp: data => ApiService.postData(AUTH_URLS.RESEND_OTP_URL, data),
+  resetPassword: data => ApiService.postData(AUTH_URLS.RESET_PASSWORD_URL, data),
 };
 
 export default AuthApiService;
