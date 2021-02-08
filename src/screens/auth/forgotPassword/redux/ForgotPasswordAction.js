54c70b295d94076181e7e6311d8f545b6fd2af5f
kanerodgers7 <jellyfish9629@gmail.com>
2021-02-08 19:07:17 +0530

    Handle validation message for forgot password screen

--- src/screens/auth/forgotPassword/redux/ForgotPasswordAction.js
+++ src/screens/auth/forgotPassword/redux/ForgotPasswordAction.js
@@ -8,17 +8,31 @@ export const forgotPassword = async email => {
     const response = await AuthApiService.forgotPassword(data);
 
     if (response.data.status === 'SUCCESS') {
-      const { id } = response.data.data;
-      SESSION_VARIABLES.USER_ID = id;
+      SESSION_VARIABLES.USER_EMAIL = email;
 
       successNotification('OTP has been sent successfully to your registered email address.');
     }
   } catch (e) {
-    if (e.response.data.status === undefined) {
-      errorNotification('It seems like server is down, Please try again later.');
-    } else if (e.response.data.status === 'INTERNAL_SERVER_ERROR') {
-      errorNotification('Internal server error');
+    if (e.response && e.response.data) {
+      if (e.response.data.status === undefined) {
+        errorNotification('It seems like server is down, Please try again later.');
+      } else if (e.response.data.status === 'INTERNAL_SERVER_ERROR') {
+        errorNotification('Internal server error');
+      } else if (e.response.data.status === 'ERROR') {
+        if (e.response.data.messageCode) {
+          switch (e.response.data.messageCode) {
+            case 'USER_NOT_FOUND':
+            case 'EMAIL_NOT_FOUND':
+              errorNotification('User not found');
+              break;
+            default:
+              break;
+          }
+        } else {
+          errorNotification(e);
+        }
+      }
+      throw Error();
     }
-    throw Error();
   }
 };
