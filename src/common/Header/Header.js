f3fca766106f7113a5e7529b5013d9b8b89f4e47
kanerodgers7 <jellyfish9629@gmail.com>
2021-02-09 22:53:04 +0530

    Handled change password and logout functionality

--- src/common/Header/Header.js
+++ src/common/Header/Header.js
@@ -1,27 +1,99 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import './Header.scss';
+import { useHistory } from 'react-router-dom';
 import BigInput from '../BigInput/BigInput';
 import dummy from '../../assets/images/dummy.svg';
 import IconButton from '../IconButton/IconButton';
 import Modal from '../Modal/Modal';
 import Input from '../Input/Input';
 import { useOnClickOutside } from '../../hooks/UserClickOutsideHook';
+import { errorNotification } from '../Toast';
+import { changePassword, logoutUser } from './redux/HeaderAction';
 
 const Header = () => {
+  const history = useHistory();
+
+  const [currentPassword, setCurrentPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
   const [showUserSettings, setShowUserSettings] = React.useState(false);
   const [showChangePasswordModal, setShowChangePasswordModal] = React.useState(false);
-  const toggleChangePasswordModal = () => setShowChangePasswordModal(e => !e);
-  const changePasswordBtns = [
-    { title: 'Close', buttonType: 'background-color', onClick: toggleChangePasswordModal },
-    { title: 'Save', buttonType: 'primary', onClick: toggleChangePasswordModal },
-  ];
+  const toggleChangePasswordModal = value =>
+    setShowChangePasswordModal(value !== undefined ? value : e => !e);
 
-  const openUserSettings = () => setShowUserSettings(true);
-  const closeUserSettings = () => setShowUserSettings(false);
+  const toggleUserSettings = value => setShowUserSettings(value !== undefined ? value : e => !e);
+
+  const resetInputs = () => {
+    setCurrentPassword('');
+    setNewPassword('');
+    setConfirmPassword('');
+  };
+
+  const onCloseChangePasswordClick = () => {
+    toggleChangePasswordModal(false);
+    resetInputs();
+  };
+
+  const onChangePasswordClick = async () => {
+    if (currentPassword.toString().trim().length === 0) {
+      return errorNotification('Please enter current password');
+    }
+    if (newPassword.toString().trim().length === 0) {
+      return errorNotification('Please enter new password');
+    }
+    if (confirmPassword.toString().trim().length === 0) {
+      return errorNotification('Please enter confirm password');
+    }
+    if (newPassword !== confirmPassword) {
+      return errorNotification('New password and confirm password should be same');
+    }
+
+    try {
+      toggleChangePasswordModal(false);
+      await changePassword(currentPassword, newPassword);
+      resetInputs();
+    } catch (e) {
+      /**/
+    }
+    return true;
+  };
+
+  const onLogoutClick = async () => {
+    try {
+      await logoutUser();
+      history.replace('login');
+      toggleUserSettings(false);
+    } catch (e) {
+      /**/
+    }
+    return true;
+  };
+
+  const changePasswordBtns = [
+    {
+      title: 'Close',
+      buttonType: 'background-color',
+      onClick: onCloseChangePasswordClick,
+    },
+    { title: 'Save', buttonType: 'primary', onClick: onChangePasswordClick },
+  ];
 
   const userSettingsRef = useRef();
 
-  useOnClickOutside(userSettingsRef, closeUserSettings);
+  useOnClickOutside(userSettingsRef, () => toggleUserSettings(false));
+
+  const onChangeCurrentPassword = e => {
+    setCurrentPassword(e.target.value);
+  };
+
+  const onChangeNewPassword = e => {
+    setNewPassword(e.target.value);
+  };
+
+  const onChangeConfirmPassword = e => {
+    setConfirmPassword(e.target.value);
+  };
 
   return (
     <div className="header-container">
@@ -40,16 +112,16 @@ const Header = () => {
           buttonType="outlined-bg"
           className="notification"
         />
-        <img className="user-dp" src={dummy} onClick={openUserSettings} />
+        <img className="user-dp" src={dummy} onClick={toggleUserSettings} />
         {showUserSettings && (
           <div ref={userSettingsRef} className="user-settings">
-            <div onClick={closeUserSettings}>
+            <div onClick={toggleUserSettings}>
               <span className="material-icons-round">edit</span> Profile
             </div>
             <div onClick={toggleChangePasswordModal}>
               <span className="material-icons-round">lock</span> Change Password
             </div>
-            <div onClick={closeUserSettings}>
+            <div onClick={onLogoutClick}>
               <span className="material-icons-round">exit_to_app</span> Logout
             </div>
           </div>
@@ -64,15 +136,30 @@ const Header = () => {
           <div className="change-password-grid">
             <div className="form-title">Current password</div>
             <div>
-              <Input type="password" placeholder="Enter Current Password" />{' '}
+              <Input
+                type="password"
+                placeholder="Enter Current Password"
+                value={currentPassword}
+                onChange={onChangeCurrentPassword}
+              />{' '}
             </div>
             <div className="form-title">New password</div>
             <div>
-              <Input type="password" placeholder="Enter New Password" />{' '}
+              <Input
+                type="password"
+                placeholder="Enter New Password"
+                value={newPassword}
+                onChange={onChangeNewPassword}
+              />{' '}
             </div>
             <div className="form-title">Re Enter password</div>
             <div>
-              <Input type="password" placeholder="Re Enter Password" />{' '}
+              <Input
+                type="password"
+                placeholder="Re Enter Password"
+                value={confirmPassword}
+                onChange={onChangeConfirmPassword}
+              />{' '}
             </div>
           </div>
         </Modal>
