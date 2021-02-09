dbd666cd164233d0311121b2d4913cd7bf774016
kanerodgers7 <jellyfish9629@gmail.com>
2021-02-09 21:45:49 +0530

    Added logout api

--- src/common/Header/services/HeaderApiService.js
+++ src/common/Header/services/HeaderApiService.js
@@ -3,6 +3,7 @@ import { HEADER_URLS } from '../../../constants/UrlConstants';
 
 const HeaderApiService = {
   changePassword: data => ApiService.putData(HEADER_URLS.CHANGE_PASSWORD_URL, data),
+  logoutUser: () => ApiService.deleteData(HEADER_URLS.LOGOUT_URL),
 };
 
 export default HeaderApiService;
