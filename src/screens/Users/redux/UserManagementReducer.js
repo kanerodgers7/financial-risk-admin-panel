fd3ce1ef2a9d9968ed24f38b021e842214eb5035
kanerodgers7 <jellyfish9629@gmail.com>
2021-02-09 10:32:57 +0530

    Added user management list reducer

---
+++ src/screens/Users/redux/UserManagementReducer.js
@@ -0,0 +1,17 @@
+import { LOGIN_REDUX_CONSTANTS } from '../../auth/login/redux/LoginReduxConstants';
+import { USER_MANAGEMENT_REDUX_CONSTANTS } from './UserManagementReduxConstants';
+
+const initialUserManagementListState = { docs: [], total: 0, limit: 0, page: 1, pages: 1 };
+
+export const userManagementList = (state = initialUserManagementListState, action) => {
+  switch (action.type) {
+    case USER_MANAGEMENT_REDUX_CONSTANTS.USER_MANAGEMENT_LIST_USER_ACTION:
+      return action.data;
+
+    case LOGIN_REDUX_CONSTANTS.LOGOUT_USER_ACTION:
+      return null;
+
+    default:
+      return state;
+  }
+};
