378246d6af396575d28a7a871fac51ddb2f85378
kanerodgers7 <jellyfish9629@gmail.com>
2021-02-11 17:49:22 +0530

    Added user role constants

---
+++ src/constants/UserlistConstants.js
@@ -0,0 +1,5 @@
+export const USER_ROLES = [
+  { label: 'Risk Analyst', value: 'riskAnalyst' },
+  { label: 'Service Manager', value: 'serviceManager' },
+  { label: 'Super Admin', value: 'superAdmin' },
+];
