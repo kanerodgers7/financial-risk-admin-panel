9c292aec60eb8d51785966ccb713f808aa2433c4
kanerodgers7 <jellyfish9629@gmail.com>
2021-02-11 18:07:43 +0530

    Added user management list by filter action

--- src/screens/Users/redux/UserManagementAction.js
+++ src/screens/Users/redux/UserManagementAction.js
@@ -31,6 +31,33 @@ export const getUserManagementList = (params = { page: 1, limit: 15 }) => {
   };
 };
 
+export const getUserManagementListByFilter = (params = { page: 1, limit: 15 }) => {
+  return async dispatch => {
+    try {
+      const response = await UserManagementApiService.getAllUserListByFilter(params);
+
+      if (response.data.status === 'SUCCESS') {
+        dispatch({
+          type: USER_MANAGEMENT_REDUX_CONSTANTS.USER_MANAGEMENT_LIST_USER_ACTION,
+          data: response.data.data,
+        });
+      }
+    } catch (e) {
+      if (e.response && e.response.data) {
+        if (e.response.data.status === undefined) {
+          errorNotification('It seems like server is down, Please try again later.');
+        } else if (e.response.data.status === 'INTERNAL_SERVER_ERROR') {
+          errorNotification('Internal server error');
+        } else if (e.response.data.status === 'ERROR') {
+          // TODO handle cases
+          errorNotification('It seems like server is down, Please try again later.');
+        }
+        throw Error();
+      }
+    }
+  };
+};
+
 export const getUserColumnListName = () => {
   return async dispatch => {
     try {
