65d9a1439b048ab32097462bfdef2a7da800c0a5
kanerodgers7 <jellyfish9629@gmail.com>
2021-02-11 18:00:17 +0530

    Added api for get user management column list

--- src/screens/Users/services/UserManagementApiService.js
+++ src/screens/Users/services/UserManagementApiService.js
@@ -3,6 +3,7 @@ import { USER_MANAGEMENT_URLS } from '../../../constants/UrlConstants';
 
 const UserManagementApiService = {
   getAllUserList: params => ApiService.getData(USER_MANAGEMENT_URLS.USER_LIST_URL, { params }),
+  getUserColumnListName: () => ApiService.getData(USER_MANAGEMENT_URLS.USER_COLUMN_NAME_LIST_URL),
   getAllUserListByFilter: params =>
     ApiService.getData(USER_MANAGEMENT_URLS.USER_LIST_BY_FILTER_URL, { params }),
 };
