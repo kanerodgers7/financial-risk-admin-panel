24989b5223dd7d2ddb2e9bb3dccc669781ca6478
kanerodgers7 <jellyfish9629@gmail.com>
2021-02-11 18:04:32 +0530

    Added user management column list reducer to root reducer

--- src/redux/RootReducer.js
+++ src/redux/RootReducer.js
@@ -1,9 +1,13 @@
 import { combineReducers } from 'redux';
 import { loggedUser } from '../screens/auth/login/redux/LoginReducer';
-import { userManagementList } from '../screens/Users/redux/UserManagementReducer';
+import {
+  userManagementColumnList,
+  userManagementList,
+} from '../screens/Users/redux/UserManagementReducer';
 
 const rootReducer = combineReducers({
   loggedUser,
+  userManagementColumnList,
   userManagementList,
 });
 export default rootReducer;
