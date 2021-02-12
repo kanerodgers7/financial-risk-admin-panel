88bd7ec1fa1baf75c0aaad61c0c19cc57591bc3f
kanerodgers7 <jellyfish9629@gmail.com>
2021-02-12 19:40:13 +0530

    Handle session case for user

--- src/helpers/LocalStorageHelper.js
+++ src/helpers/LocalStorageHelper.js
@@ -8,24 +8,37 @@ export const saveAuthTokenLocalStorage = authToken => {
 };
 
 export const saveTokenToSession = authToken => {
+  sessionStorage.setItem(AUTH_TOKEN, authToken);
   SESSION_VARIABLES.USER_TOKEN = authToken;
 };
 
 export const saveTokenFromLocalStorageToSession = () => {
-  const authToken = localStorage.getItem(AUTH_TOKEN);
-  if (authToken) {
-    SESSION_VARIABLES.USER_TOKEN = authToken;
+  const authTokenFromSession = sessionStorage.getItem(AUTH_TOKEN);
+
+  if (authTokenFromSession) {
+    SESSION_VARIABLES.USER_TOKEN = authTokenFromSession;
+  } else {
+    const authToken = localStorage.getItem(AUTH_TOKEN);
+    if (authToken) {
+      SESSION_VARIABLES.USER_TOKEN = authToken;
+    }
   }
 };
 
 export const getAuthTokenLocalStorage = () => {
+  const tokenFromSession = sessionStorage.getItem(AUTH_TOKEN);
   if (SESSION_VARIABLES.USER_TOKEN) {
     return SESSION_VARIABLES.USER_TOKEN;
+  }
+  if (tokenFromSession) {
+    SESSION_VARIABLES.USER_TOKEN = tokenFromSession;
+    return tokenFromSession;
   }
   return localStorage.getItem(AUTH_TOKEN);
 };
 
 export const clearAuthToken = () => {
   SESSION_VARIABLES.USER_TOKEN = null;
+  sessionStorage.removeItem(AUTH_TOKEN);
   localStorage.removeItem(AUTH_TOKEN);
 };
