35bed839394422ee25de0cb94d6566ef95e926fa
kanerodgers7 <jellyfish9629@gmail.com>
2021-02-12 16:46:24 +0530

    Added user module access list

--- src/constants/UserlistConstants.js
+++ src/constants/UserlistConstants.js
@@ -3,3 +3,9 @@ export const USER_ROLES = [
   { label: 'Service Manager', value: 'serviceManager' },
   { label: 'Super Admin', value: 'superAdmin' },
 ];
+
+export const USER_MODULE_ACCESS = [
+  { label: 'Read Access', value: 'read' },
+  { label: 'Write Access', value: 'write' },
+  { label: 'All Access', value: 'full-access' },
+];
