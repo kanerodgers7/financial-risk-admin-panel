2bc0cea11d08d96a89040223a60e4ab0c31acefd
kanerodgers7 <jellyfish9629@gmail.com>
2021-02-12 10:13:10 +0530

    Added update column list name api

--- src/screens/Users/services/UserManagementApiService.js
+++ src/screens/Users/services/UserManagementApiService.js
@@ -4,6 +4,8 @@ import { USER_MANAGEMENT_URLS } from '../../../constants/UrlConstants';
 const UserManagementApiService = {
   getAllUserList: params => ApiService.getData(USER_MANAGEMENT_URLS.USER_LIST_URL, { params }),
   getUserColumnListName: () => ApiService.getData(USER_MANAGEMENT_URLS.USER_COLUMN_NAME_LIST_URL),
+  updateUserColumnListName: data =>
+    ApiService.putData(USER_MANAGEMENT_URLS.UPDATE_USER_COLUMN_NAME_LIST_URL, data),
   getAllUserListByFilter: params =>
     ApiService.getData(USER_MANAGEMENT_URLS.USER_LIST_BY_FILTER_URL, { params }),
 };
