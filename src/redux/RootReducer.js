480cf7ffdabe0db5b063d7a9bb9be0a44e36426f
kanerodgers7 <jellyfish9629@gmail.com>
2021-02-12 14:54:44 +0530

    Added selected user data to root reducer

--- src/redux/RootReducer.js
+++ src/redux/RootReducer.js
@@ -1,12 +1,17 @@
-import {combineReducers} from 'redux';
-import {loggedUser} from '../screens/auth/login/redux/LoginReducer';
-import {userManagementColumnList, userManagementList,} from '../screens/Users/redux/UserManagementReducer';
-import {clientList} from '../screens/Clients/redux/ClientReducer';
+import { combineReducers } from 'redux';
+import { loggedUser } from '../screens/auth/login/redux/LoginReducer';
+import {
+  selectedUserData,
+  userManagementColumnList,
+  userManagementList,
+} from '../screens/Users/redux/UserManagementReducer';
+import { clientList } from '../screens/Clients/redux/ClientReducer';
 
 const rootReducer = combineReducers({
   loggedUser,
   userManagementColumnList,
   userManagementList,
   clientList,
+  selectedUserData,
 });
 export default rootReducer;
