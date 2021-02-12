283e14a64e54935163305fdb75024eff0cb93b9f
kanerodgers7 <jellyfish9629@gmail.com>
2021-02-12 14:53:03 +0530

    Added api to get selected user data

--- src/screens/Users/services/UserManagementApiService.js
+++ src/screens/Users/services/UserManagementApiService.js
@@ -4,6 +4,8 @@ import { USER_MANAGEMENT_URLS } from '../../../constants/UrlConstants';
 const UserManagementApiService = {
   getAllUserList: params => ApiService.getData(USER_MANAGEMENT_URLS.USER_LIST_URL, { params }),
   getUserColumnListName: () => ApiService.getData(USER_MANAGEMENT_URLS.USER_COLUMN_NAME_LIST_URL),
+  getSelectedUserData: id =>
+    ApiService.getData(`${USER_MANAGEMENT_URLS.SELECTED_USER_DETAILS_URL}${id}`),
   updateUserColumnListName: data =>
     ApiService.putData(USER_MANAGEMENT_URLS.UPDATE_USER_COLUMN_NAME_LIST_URL, data),
   getAllUserListByFilter: params =>
