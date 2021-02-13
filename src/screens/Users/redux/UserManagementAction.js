f14bfaaff886030a5da5ae603b9eb9651e07ccfa
kanerodgers7 <jellyfish9629@gmail.com>
2021-02-13 11:53:36 +0530

    Removed user management list action

--- src/screens/Users/redux/UserManagementAction.js
+++ src/screens/Users/redux/UserManagementAction.js
@@ -7,32 +7,6 @@ import {
   USER_MANAGEMENT_REDUX_CONSTANTS,
 } from './UserManagementReduxConstants';
 
-export const getUserManagementList = (params = { page: 1, limit: 15 }) => {
-  return async dispatch => {
-    try {
-      const response = await UserManagementApiService.getAllUserList(params);
-
-      if (response.data.status === 'SUCCESS') {
-        dispatch({
-          type: USER_MANAGEMENT_REDUX_CONSTANTS.USER_MANAGEMENT_LIST_USER_ACTION,
-          data: response.data.data,
-        });
-      }
-    } catch (e) {
-      if (e.response && e.response.data) {
-        if (e.response.data.status === undefined) {
-          errorNotification('It seems like server is down, Please try again later.');
-        } else if (e.response.data.status === 'INTERNAL_SERVER_ERROR') {
-          errorNotification('Internal server error');
-        } else if (e.response.data.status === 'ERROR') {
-          errorNotification('It seems like server is down, Please try again later.');
-        }
-        throw Error();
-      }
-    }
-  };
-};
-
 export const getUserManagementListByFilter = (params = { page: 1, limit: 15 }) => {
   return async dispatch => {
     try {
