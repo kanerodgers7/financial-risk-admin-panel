530fd8b60c2f21d542e198461f605c3d5c4c59a9
kanerodgers7 <jellyfish9629@gmail.com>
2021-02-13 13:23:27 +0530

    Modified useQueryParams hook

--- src/hooks/GetQueryParamHook.js
+++ src/hooks/GetQueryParamHook.js
@@ -1,5 +1,11 @@
 import { useLocation } from 'react-router-dom';
 
 export const useQueryParams = () => {
-  return new URLSearchParams(useLocation().search);
+  const search = new URLSearchParams(useLocation().search);
+  const params = {};
+  Array.from(search.entries()).forEach(([k, v]) => {
+    params[`${k}`] = v;
+  });
+
+  return params;
 };
