78da94d63d67e60598c7028906f4e012ad58de15
kanerodgers7 <jellyfish9629@gmail.com>
2021-02-17 10:37:34 +0530

    WIP sidebar constants

---
+++ src/constants/SidebarConstants.js
@@ -0,0 +1,74 @@
+export const SIDEBAR_URLS = [
+  {
+    label: 'Dashboard',
+    title: 'Dashboard',
+    icon: 'insert_chart',
+    url: '/dashboard',
+  },
+  {
+    label: 'My Work',
+    title: 'Dashboard',
+    icon: 'event_available',
+    url: '/my-work',
+  },
+  {
+    label: 'Application',
+    title: 'Dashboard',
+    icon: 'insert_chart',
+    url: '/dashboard',
+  },
+  {
+    label: 'Clients',
+    title: 'Dashboard',
+    icon: 'insert_chart',
+    url: '/dashboard',
+  },
+  {
+    label: 'Debtors',
+    title: 'Dashboard',
+    icon: 'insert_chart',
+    url: '/dashboard',
+  },
+  {
+    label: 'Reports',
+    title: 'Dashboard',
+    icon: 'insert_chart',
+    url: '/dashboard',
+  },
+  {
+    label: 'Insurer',
+    title: 'Dashboard',
+    icon: 'insert_chart',
+    url: '/dashboard',
+  },
+  {
+    label: 'Users',
+    title: 'Dashboard',
+    icon: 'insert_chart',
+    url: '/user',
+  },
+  {
+    label: 'Claims',
+    title: 'Dashboard',
+    icon: 'insert_chart',
+    url: '/dashboard',
+  },
+  {
+    label: 'Overdues',
+    title: 'Dashboard',
+    icon: 'insert_chart',
+    url: '/dashboard',
+  },
+  {
+    label: 'Settings',
+    title: 'Dashboard',
+    icon: 'insert_chart',
+    url: '/dashboard',
+  },
+  {
+    label: 'Audit Log',
+    title: 'Dashboard',
+    icon: 'insert_chart',
+    url: '/dashboard',
+  },
+];
