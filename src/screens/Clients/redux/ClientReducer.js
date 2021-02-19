529703b2a893f2ac9d717d7042f29177f3135bb2
kanerodgers7 <jellyfish9629@gmail.com>
2021-02-19 17:26:35 +0530

    Eslint fix client reducer

--- src/screens/Clients/redux/ClientReducer.js
+++ src/screens/Clients/redux/ClientReducer.js
@@ -1,20 +1,23 @@
 import { LOGIN_REDUX_CONSTANTS } from '../../auth/login/redux/LoginReduxConstants';
 import { CLIENT_REDUX_CONSTANTS } from './ClientReduxConstants';
 
-const initialClientListState = { clientList: {docs: [], total: 0, limit: 0, page: 1, pages: 1}, selectedClient: null, };
+const initialClientListState = {
+  clientList: { docs: [], total: 0, limit: 0, page: 1, pages: 1 },
+  selectedClient: null,
+};
 
 export const clientManagement = (state = initialClientListState, action) => {
   switch (action.type) {
     case CLIENT_REDUX_CONSTANTS.CLIENT_LIST_USER_ACTION:
       return {
         ...state,
-        clientList: action.data
+        clientList: action.data,
       };
 
     case CLIENT_REDUX_CONSTANTS.SELECTED_CLIENT_DATA:
       return {
         ...state,
-        selectedClient: action.data
+        selectedClient: action.data,
       };
 
     case LOGIN_REDUX_CONSTANTS.LOGOUT_USER_ACTION:
