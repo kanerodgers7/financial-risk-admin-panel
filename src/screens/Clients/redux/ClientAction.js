e78c478fd8feb114d32cb993844468207f6de364
kanerodgers7 <jellyfish9629@gmail.com>
2021-02-23 19:44:58 +0530

    Added actions for client contact list

--- src/screens/Clients/redux/ClientAction.js
+++ src/screens/Clients/redux/ClientAction.js
@@ -1,6 +1,7 @@
 import { errorNotification } from '../../../common/Toast';
 import ClientApiService from '../services/ClientApiService';
 import { CLIENT_REDUX_CONSTANTS } from './ClientReduxConstants';
+import ClientContactApiService from '../services/ClientContactApiService';
 
 export const getClientList = (params = { page: 1, limit: 15 }) => {
   return async dispatch => {
@@ -47,6 +48,62 @@ export const getClientById = id => {
           errorNotification('Internal server error');
         } else if (e.response.data.status === 'ERROR') {
           // TODO handle cases
+          errorNotification('It seems like server is down, Please try again later.');
+        }
+        throw Error();
+      }
+    }
+  };
+};
+
+/*
+ * Contact section
+ * */
+
+export const getClientContactListData = () => {
+  return async dispatch => {
+    try {
+      const response = await ClientContactApiService.getClientContactList();
+
+      if (response.data.status === 'SUCCESS') {
+        dispatch({
+          type: CLIENT_REDUX_CONSTANTS.CONTACT.CLIENT_CONTACT_LIST_USER_ACTION,
+          data: response.data.data,
+        });
+      }
+    } catch (e) {
+      if (e.response && e.response.data) {
+        if (e.response.data.status === undefined) {
+          errorNotification('It seems like server is down, Please try again later.');
+        } else if (e.response.data.status === 'INTERNAL_SERVER_ERROR') {
+          errorNotification('Internal server error');
+        } else if (e.response.data.status === 'ERROR') {
+          errorNotification('It seems like server is down, Please try again later.');
+        }
+        throw Error();
+      }
+    }
+  };
+};
+
+export const getClientContactColumnNamesList = () => {
+  return async dispatch => {
+    try {
+      const response = await ClientContactApiService.getClientContactColumnListName();
+
+      if (response.data.status === 'SUCCESS') {
+        dispatch({
+          type: CLIENT_REDUX_CONSTANTS.CONTACT.CLIENT_CONTACT_COLUMN_LIST_USER_ACTION,
+          data: response.data.data,
+        });
+      }
+    } catch (e) {
+      if (e.response && e.response.data) {
+        if (e.response.data.status === undefined) {
+          errorNotification('It seems like server is down, Please try again later.');
+        } else if (e.response.data.status === 'INTERNAL_SERVER_ERROR') {
+          errorNotification('Internal server error');
+        } else if (e.response.data.status === 'ERROR') {
           errorNotification('It seems like server is down, Please try again later.');
         }
         throw Error();
