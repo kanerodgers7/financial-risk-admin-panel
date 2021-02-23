bfb08f23ba8607f3f3fab8bac7eb8f0d86543c7f
kanerodgers7 <jellyfish9629@gmail.com>
2021-02-23 19:44:32 +0530

    Added api for client contact list and columns

---
+++ src/screens/Clients/services/ClientContactApiService.js
@@ -0,0 +1,10 @@
+import ApiService from '../../../services/api-service/ApiService';
+import { CLIENT_URLS } from '../../../constants/UrlConstants';
+
+const ClientContactApiService = {
+  getClientContactList: () => ApiService.getData(CLIENT_URLS.CONTACT.CONTACT_LIST),
+  getClientContactColumnListName: () =>
+    ApiService.getData(CLIENT_URLS.CONTACT.COLUMN_NAME_LIST_URL),
+};
+
+export default ClientContactApiService;
