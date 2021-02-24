b78ff5cb6e0164786e3e618a18bdb06554963fb0
kanerodgers7 <jellyfish9629@gmail.com>
2021-02-24 15:15:01 +0530

    Added update selected client data api

--- src/screens/Clients/services/ClientApiService.js
+++ src/screens/Clients/services/ClientApiService.js
@@ -4,6 +4,8 @@ import { CLIENT_URLS } from '../../../constants/UrlConstants';
 const ClientApiService = {
   getAllClientList: params => ApiService.getData(CLIENT_URLS.CLIENT_LIST_URL, { params }),
   getClientById: id => ApiService.getData(CLIENT_URLS.CLIENT_BY_ID_URL + id),
+  updateSelectedClientData: (id, data) =>
+    ApiService.putData(CLIENT_URLS.CLIENT_BY_ID_URL + id, data),
 };
 
 export default ClientApiService;
