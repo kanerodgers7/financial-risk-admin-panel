ba1cd1eed0cc2cdc5005fcdd4b659756d12eb7eb
kanerodgers7 <jellyfish9629@gmail.com>
2021-02-25 20:03:20 +0530

    Renamed api for table action

--- src/common/Table/TableApiService.js
+++ src/common/Table/TableApiService.js
@@ -2,7 +2,7 @@ import ApiService from '../../services/api-service/ApiService';
 import { BASE_URL } from '../../constants/UrlConstants';
 
 const TableApiService = {
-  getDrawerData: ({ url, id, method, params = {} }) => {
+  tableActions: ({ url, id, method, params = {} }) => {
     console.log(`${BASE_URL}${url}/${id}`);
     return ApiService.request({ url: `${BASE_URL}${url}/${id}`, method, params });
   },
