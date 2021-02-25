36b3f5a74af093bfab21d6b9670b7357a0580c60
kanerodgers7 <jellyfish9629@gmail.com>
2021-02-25 20:04:35 +0530

    Added data in table action api

--- src/common/Table/TableApiService.js
+++ src/common/Table/TableApiService.js
@@ -2,10 +2,8 @@ import ApiService from '../../services/api-service/ApiService';
 import { BASE_URL } from '../../constants/UrlConstants';
 
 const TableApiService = {
-  tableActions: ({ url, id, method, params = {} }) => {
-    console.log(`${BASE_URL}${url}/${id}`);
-    return ApiService.request({ url: `${BASE_URL}${url}/${id}`, method, params });
-  },
+  tableActions: ({ url, id, method, params = {}, data }) =>
+    ApiService.request({ url: `${BASE_URL}${url}/${id}`, method, params, data }),
 };
 
 export default TableApiService;
