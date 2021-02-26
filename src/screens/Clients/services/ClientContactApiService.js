acf27d29c55fb19ca39bbe07218dd830304e26bd
kanerodgers7 <jellyfish9629@gmail.com>
2021-02-26 18:36:24 +0530

    Added client policies sync api

--- src/screens/Clients/services/ClientContactApiService.js
+++ src/screens/Clients/services/ClientContactApiService.js
@@ -8,6 +8,8 @@ const ClientContactApiService = {
     ApiService.getData(CLIENT_URLS.CONTACT.COLUMN_NAME_LIST_URL),
   updateClientContactColumnListName: data =>
     ApiService.putData(CLIENT_URLS.CONTACT.COLUMN_NAME_LIST_URL, data),
+  syncClientContactData: id =>
+    ApiService.putData(`${CLIENT_URLS.CONTACT.SYNC_CLIENT_CONTACT_DATA_URL}${id}`),
 };
 
 export default ClientContactApiService;
