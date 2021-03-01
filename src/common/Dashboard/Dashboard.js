304b5e6bd01268156aef856243cb3432c84840a5
kanerodgers7 <jellyfish9629@gmail.com>
2021-03-01 17:13:16 +0530

    Get all user privileges in dashboard

--- src/common/Dashboard/Dashboard.js
+++ src/common/Dashboard/Dashboard.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Dashboard.scss';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
 import SideMenu from '../SideMenu/SideMenu';
 import Header from '../Header/Header';
+import { getAllUserPrivileges } from '../../screens/Users/redux/UserManagementAction';
 
 const Dashboard = props => {
   const { children } = props;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllUserPrivileges());
+  }, []);
+
   return (
     <div className="dashboard-container">
       <SideMenu />
