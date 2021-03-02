5e87923943961d2ca091804c2e9d3d43932c70ed
kanerodgers7 <jellyfish9629@gmail.com>
2021-03-02 20:57:25 +0530

    Added add, update and delete api service for client notes

--- src/screens/Clients/services/ClientNotesApiService.js
+++ src/screens/Clients/services/ClientNotesApiService.js
@@ -4,6 +4,9 @@ import { CLIENT_URLS } from '../../../constants/UrlConstants';
 const ClientNotesApiService = {
   getClientNotesList: (id, params) =>
     ApiService.getData(`${CLIENT_URLS.NOTES.NOTES_LIST}${id}`, { params }),
+  addClientNote: data => ApiService.postData(`${CLIENT_URLS.NOTES.NOTES_LIST}`, data),
+  updateClientNote: (id, data) => ApiService.putData(`${CLIENT_URLS.NOTES.NOTES_LIST}${id}`, data),
+  deleteClientNote: id => ApiService.deleteData(`${CLIENT_URLS.NOTES.NOTES_LIST}${id}`),
 };
 
 export default ClientNotesApiService;
