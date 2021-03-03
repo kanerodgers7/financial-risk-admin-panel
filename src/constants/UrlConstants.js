ec3c6fdd3b5e7aeeecd75e21c80f5c7490595ff5
kanerodgers7 <jellyfish9629@gmail.com>
2021-03-03 14:50:25 +0530

    Url changes for rp in base url

--- src/constants/UrlConstants.js
+++ src/constants/UrlConstants.js
@@ -1,61 +1,60 @@
-export const BASE_URL = 'https://client.trad.dev.gradlesol.com/app/';
-// export const BASE_URL = 'https://69f29e402623.ngrok.io/';
+export const BASE_URL = 'https://client.trad.dev.gradlesol.com/app/rp/';
 
 export const AUTH_URLS = {
-  LOGIN_URL: `${BASE_URL}rp/auth/login/`,
-  FORGOT_PASSWORD_URL: `${BASE_URL}rp/auth/forget-password/`,
-  VERIFY_OTP_URL: `${BASE_URL}rp/auth/verify-otp/`,
-  RESEND_OTP_URL: `${BASE_URL}rp/auth/resend-otp/`,
-  RESET_PASSWORD_URL: `${BASE_URL}rp/auth/reset-password`,
+  LOGIN_URL: `${BASE_URL}auth/login/`,
+  FORGOT_PASSWORD_URL: `${BASE_URL}auth/forget-password/`,
+  VERIFY_OTP_URL: `${BASE_URL}auth/verify-otp/`,
+  RESEND_OTP_URL: `${BASE_URL}auth/resend-otp/`,
+  RESET_PASSWORD_URL: `${BASE_URL}auth/reset-password`,
 };
 
 export const HEADER_URLS = {
-  CHANGE_PASSWORD_URL: `${BASE_URL}rp/auth/change-password/`,
-  LOGOUT_URL: `${BASE_URL}rp/auth/logout/`,
+  CHANGE_PASSWORD_URL: `${BASE_URL}auth/change-password/`,
+  LOGOUT_URL: `${BASE_URL}auth/logout/`,
 };
 
 export const USER_MANAGEMENT_URLS = {
-  USER_LIST_URL: `${BASE_URL}rp/user/`,
-  USER_PRIVILEGES_URL: `${BASE_URL}rp/privilege/`,
-  SELECTED_USER_DETAILS_URL: `${BASE_URL}rp/user/`,
-  USER_COLUMN_NAME_LIST_URL: `${BASE_URL}rp/user/column-name/`,
-  USER_CLIENT_LIST_URL: `${BASE_URL}rp/user/client-name/`,
-  UPDATE_USER_COLUMN_NAME_LIST_URL: `${BASE_URL}rp/user/column-name/`,
+  USER_LIST_URL: `${BASE_URL}user/`,
+  USER_PRIVILEGES_URL: `${BASE_URL}privilege/`,
+  SELECTED_USER_DETAILS_URL: `${BASE_URL}user/`,
+  USER_COLUMN_NAME_LIST_URL: `${BASE_URL}user/column-name/`,
+  USER_CLIENT_LIST_URL: `${BASE_URL}user/client-name/`,
+  UPDATE_USER_COLUMN_NAME_LIST_URL: `${BASE_URL}user/column-name/`,
 };
 
 export const ORGANISATION_MODULE_URLS = {
-  GET_ORGANIZATION_MODULE_LIST_URL: `${BASE_URL}rp/organization/module`,
+  GET_ORGANIZATION_MODULE_LIST_URL: `${BASE_URL}organization/module`,
 };
 
 export const CLIENT_URLS = {
-  CLIENT_LIST_URL: `${BASE_URL}rp/client/`,
-  CLIENT_BY_ID_URL: `${BASE_URL}rp/client/`,
-  CLIENT_COLUMN_NAME_LIST_URL: `${BASE_URL}rp/client/column-name`,
-  UPDATE_CLIENT_COLUMN_NAME_LIST_URL: `${BASE_URL}rp/client/column-name`,
-  CLIENT_FILTER_LIST_URL: `${BASE_URL}rp/client/user-list`,
-  SYNC_CLIENT_DATA_URL: `${BASE_URL}rp/client/sync-from-crm`,
+  CLIENT_LIST_URL: `${BASE_URL}client/`,
+  CLIENT_BY_ID_URL: `${BASE_URL}client/`,
+  CLIENT_COLUMN_NAME_LIST_URL: `${BASE_URL}client/column-name`,
+  UPDATE_CLIENT_COLUMN_NAME_LIST_URL: `${BASE_URL}client/column-name`,
+  CLIENT_FILTER_LIST_URL: `${BASE_URL}client/user-list`,
+  SYNC_CLIENT_DATA_URL: `${BASE_URL}client/sync-from-crm`,
 
   CONTACT: {
-    CONTACT_LIST: `${BASE_URL}rp/client/user/`,
-    COLUMN_NAME_LIST_URL: `${BASE_URL}rp/client/user/column-name/`,
-    SYNC_CLIENT_CONTACT_DATA_URL: `${BASE_URL}rp/client/user/sync-from-crm/`,
+    CONTACT_LIST: `${BASE_URL}client/user/`,
+    COLUMN_NAME_LIST_URL: `${BASE_URL}client/user/column-name/`,
+    SYNC_CLIENT_CONTACT_DATA_URL: `${BASE_URL}client/user/sync-from-crm/`,
   },
 
   POLICIES: {
-    POLICIES_LIST: `${BASE_URL}rp/policy/`,
-    COLUMN_NAME_LIST_URL: `${BASE_URL}rp/policy/column-name/`,
-    SYNC_CLIENT_POLICIES_DATA_URL: `${BASE_URL}rp/policy/client/sync-from-crm/`,
+    POLICIES_LIST: `${BASE_URL}policy/`,
+    COLUMN_NAME_LIST_URL: `${BASE_URL}policy/column-name/`,
+    SYNC_CLIENT_POLICIES_DATA_URL: `${BASE_URL}policy/client/sync-from-crm/`,
   },
 
   NOTES: {
-    NOTES_LIST: `${BASE_URL}rp/note/`,
+    NOTES_LIST: `${BASE_URL}note/`,
   },
 };
 
 export const INSURER_URLS = {
-  INSURER_LIST_URL: `${BASE_URL}rp/insurer/`,
-  SELECTED_INSURER_DETAILS_URL: `${BASE_URL}rp/insurer/`,
-  INSURER_COLUMN_NAME_LIST_URL: `${BASE_URL}rp/insurer/column-name/`,
-  UPDATE_INSURER_COLUMN_NAME_LIST_URL: `${BASE_URL}rp/user/column-name/`,
-  INSURER_LIST_BY_FILTER_URL: `${BASE_URL}rp/insurer/filter/`,
+  INSURER_LIST_URL: `${BASE_URL}insurer/`,
+  SELECTED_INSURER_DETAILS_URL: `${BASE_URL}insurer/`,
+  INSURER_COLUMN_NAME_LIST_URL: `${BASE_URL}insurer/column-name/`,
+  UPDATE_INSURER_COLUMN_NAME_LIST_URL: `${BASE_URL}user/column-name/`,
+  INSURER_LIST_BY_FILTER_URL: `${BASE_URL}insurer/filter/`,
 };
