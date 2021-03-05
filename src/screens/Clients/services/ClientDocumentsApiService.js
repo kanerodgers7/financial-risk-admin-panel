adc8f5a0e97730ea5ad15591b35e677b8bc7148d
kanerodgers7 <jellyfish9629@gmail.com>
2021-03-05 14:50:11 +0530

    Update column list for documents in client model api service updated

--- src/screens/Clients/services/ClientDocumentsApiService.js
+++ src/screens/Clients/services/ClientDocumentsApiService.js
@@ -6,5 +6,7 @@ const ClientDocumentsApiService = {
     ApiService.getData(`${CLIENT_URLS.DOCUMENTS.DOCUMENTS_LIST}${id}`, { params }),
   getClientDocumentsColumnNamesList: () =>
     ApiService.getData(`${CLIENT_URLS.DOCUMENTS.COLUMN_NAME_LIST_URL}?columnFor=client-document`),
+  updateClientDocumentColumnListName: data =>
+    ApiService.putData(`${CLIENT_URLS.DOCUMENTS.COLUMN_NAME_LIST_URL}`, data),
 };
 export default ClientDocumentsApiService;
