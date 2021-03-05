c55f19da92f77214adf62f63c2d40ac62e6768f6
kanerodgers7 <jellyfish9629@gmail.com>
2021-03-05 17:44:12 +0530

    Added application company dropdown data action

--- src/screens/Application/redux/ApplicationAction.js
+++ src/screens/Application/redux/ApplicationAction.js
@@ -4,6 +4,7 @@ import {
   APPLICATION_COLUMN_LIST_REDUX_CONSTANTS,
   APPLICATION_REDUX_CONSTANTS,
 } from './ApplicationReduxConstants';
+import ApplicationCompanyStepApiServices from '../services/ApplicationCompanyStepApiServices';
 
 export const getApplicationsListByFilter = (params = { page: 1, limit: 15 }) => {
   return async dispatch => {
@@ -63,5 +64,35 @@ export const changeApplicationColumnNameList = data => {
       type: APPLICATION_COLUMN_LIST_REDUX_CONSTANTS.UPDATE_APPLICATION_COLUMN_LIST_ACTION,
       data,
     });
+  };
+};
+
+/*
+ * Contact
+ * */
+
+export const getApplicationCompanyDropDownData = () => {
+  return async dispatch => {
+    try {
+      const response = await ApplicationCompanyStepApiServices.getApplicationCompanyStepDropdownData();
+
+      if (response.data.status === 'SUCCESS') {
+        dispatch({
+          type: APPLICATION_REDUX_CONSTANTS.COMPANY.APPLICATION_COMPANY_DROP_DOWN_DATA,
+          data: response.data.data,
+        });
+      }
+    } catch (e) {
+      if (e.response && e.response.data) {
+        if (e.response.data.status === undefined) {
+          errorNotification('It seems like server is down, Please try again later.');
+        } else if (e.response.data.status === 'INTERNAL_SERVER_ERROR') {
+          errorNotification('Internal server error');
+        } else if (e.response.data.status === 'ERROR') {
+          errorNotification('It seems like server is down, Please try again later.');
+        }
+        throw Error();
+      }
+    }
   };
 };
