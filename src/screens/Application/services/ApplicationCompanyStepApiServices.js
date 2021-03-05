5a7255f8b3a0c6553c95c716d29b332ba2e2e2d5
kanerodgers7 <jellyfish9629@gmail.com>
2021-03-05 17:43:23 +0530

    Added application company get debtor details api

--- src/screens/Application/services/ApplicationCompanyStepApiServices.js
+++ src/screens/Application/services/ApplicationCompanyStepApiServices.js
@@ -4,5 +4,7 @@ import { APPLICATION_URLS } from '../../../constants/UrlConstants';
 const ApplicationCompanyStepApiServices = {
   getApplicationCompanyStepDropdownData: params =>
     ApiService.getData(APPLICATION_URLS.COMPANY.DROP_DOWN_DATA_URL, { params }),
+  getApplicationCompanyDataFromDebtor: id =>
+    ApiService.getData(`${APPLICATION_URLS.COMPANY.GET_DEBTOR_DETAILS}${id}`),
 };
 export default ApplicationCompanyStepApiServices;
