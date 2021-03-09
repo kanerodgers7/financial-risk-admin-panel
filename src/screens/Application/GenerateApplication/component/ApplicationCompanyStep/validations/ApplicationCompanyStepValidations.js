2dc3497e081fc444a586031d39fb9b54c8192f1d
kanerodgers7 <jellyfish9629@gmail.com>
2021-03-09 18:06:46 +0530

    Handle save data after validation

--- src/screens/Application/GenerateApplication/component/ApplicationCompanyStep/validations/ApplicationCompanyStepValidations.js
+++ src/screens/Application/GenerateApplication/component/ApplicationCompanyStep/validations/ApplicationCompanyStepValidations.js
@@ -1,4 +1,5 @@
 import { errorNotification } from '../../../../../../common/Toast';
+import { saveApplicationStepDataToBackend } from '../../../../redux/ApplicationAction';
 
 export const applicationCompanyStepValidations = data => {
   let validated = false;
@@ -9,6 +10,51 @@ export const applicationCompanyStepValidations = data => {
   } else if (!data.entityType || data.entityType.length <= 0) {
     errorNotification('Please select entity type');
   } else {
+    const {
+      client,
+      postcode,
+      state,
+      suburb,
+      streetType,
+      streetName,
+      streetNumber,
+      unitNumber,
+      property,
+      entityType,
+      phoneNumber,
+      entityName,
+      acn,
+      abn,
+      tradingName,
+      debtor,
+    } = data;
+
+    const finalData = {
+      stepper: 'company',
+      clientId: client[0]?.value,
+      debtorId: debtor[0]?.value,
+      abn,
+      acn,
+      entityName,
+      tradingName,
+      contactNumber: phoneNumber,
+      entityType: entityType[0]?.value,
+      address: {
+        property,
+        unitNumber,
+        streetNumber,
+        streetName,
+        streetType: streetType[0]?.value,
+        suburb,
+        state: state[0]?.value,
+        country: '',
+        postCode: postcode,
+      },
+      applicationId: '',
+    };
+
+    saveApplicationStepDataToBackend(finalData);
+
     validated = true;
   }
   return validated;
