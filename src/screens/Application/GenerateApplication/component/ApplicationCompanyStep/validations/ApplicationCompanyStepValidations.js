ff3127430c047d962cd7763644dd6e9084d7d083
kanerodgers7 <jellyfish9629@gmail.com>
2021-03-15 21:00:19 +0530

    Changes in validation for abn and acn

--- src/screens/Application/GenerateApplication/component/ApplicationCompanyStep/validations/ApplicationCompanyStepValidations.js
+++ src/screens/Application/GenerateApplication/component/ApplicationCompanyStep/validations/ApplicationCompanyStepValidations.js
@@ -4,9 +4,13 @@ import { saveApplicationStepDataToBackend } from '../../../../redux/ApplicationA
 export const applicationCompanyStepValidations = data => {
   let validated = false;
   if (!data.abn || data.abn.trim().length <= 0) {
-    errorNotification('Please select abn');
+    errorNotification('Please enter ABN number');
+  } else if (data.abn && data.abn.trim().length < 11) {
+    errorNotification('Please enter valid ABN number');
+  } else if (data.acn && data.acn.trim().length < 9) {
+    errorNotification('Please enter valid ACN number');
   } else if (!data.entityName || data.entityName.trim().length <= 0) {
-    errorNotification('Please select entity name');
+    errorNotification('Please enter entity name');
   } else if (!data.entityType || data.entityType.length <= 0) {
     errorNotification('Please select entity type');
   } else {
