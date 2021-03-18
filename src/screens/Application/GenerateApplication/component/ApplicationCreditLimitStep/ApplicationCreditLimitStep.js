a0dfd49e3a8e5252c30978aa2361ab580aef73c8
kanerodgers7 <jellyfish9629@gmail.com>
2021-03-18 10:05:51 +0530

    Handle state in credit limit state

--- src/screens/Application/GenerateApplication/component/ApplicationCreditLimitStep/ApplicationCreditLimitStep.js
+++ src/screens/Application/GenerateApplication/component/ApplicationCreditLimitStep/ApplicationCreditLimitStep.js
@@ -1,31 +1,95 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import './ApplicationCreditLimitStep.scss';
+import { useDispatch, useSelector } from 'react-redux';
 import RadioButton from '../../../../../common/RadioButton/RadioButton';
 import Input from '../../../../../common/Input/Input';
+import { updateEditApplicationField } from '../../../redux/ApplicationAction';
 
 const ApplicationCreditLimitStep = () => {
+  const dispatch = useDispatch();
+
+  const {
+    isExtendedPaymentTerms,
+    extendedPaymentTermsDetails,
+    isPassedOverdueAmount,
+    passedOverdueDetails,
+    creditLimit,
+  } = useSelector(({ application }) => application.editApplication.creditLimitStep);
+
+  const updateSingleCompanyState = useCallback((name, value) => {
+    dispatch(updateEditApplicationField('creditLimitStep', name, value));
+  }, []);
+
+  const handleInputChange = useCallback(
+    e => {
+      const { name, value } = e.target;
+      updateSingleCompanyState(name, value);
+    },
+    [updateSingleCompanyState]
+  );
+
   return (
     <>
       <div className="f-14 font-primary mb-10">
         Any extended payment terms outside your policy standard terms? *
       </div>
-      <RadioButton id="any-extended-pay-yes" name="extended_payment" value="yes" label="Yes" />
-      <RadioButton id="any-extended-pay-no" name="extended_payment" value="no" label="No" />
+      <RadioButton
+        id="any-extended-pay-yes"
+        name="isExtendedPaymentTerms"
+        label="Yes"
+        value="yes"
+        checked={isExtendedPaymentTerms === 'yes'}
+        onChange={handleInputChange}
+      />
+      <RadioButton
+        id="any-extended-pay-no"
+        name="isExtendedPaymentTerms"
+        label="No"
+        value="no"
+        checked={isExtendedPaymentTerms === 'no'}
+        onChange={handleInputChange}
+      />
 
       <div className="if-yes-row">
         <span className="font-primary mr-15">If yes, please provide details</span>
-        <Input type="text" placeholder="Details" />
+        <Input
+          type="text"
+          placeholder="Details"
+          name="extendedPaymentTermsDetails"
+          value={extendedPaymentTermsDetails}
+          onChange={handleInputChange}
+        />
       </div>
 
       <div className="f-14 font-primary mb-10">
         Any overdue amounts passed your maximum extension period / Credit period? *
       </div>
-      <RadioButton id="passed-max-period-yes" name="overdue_amounts" label="Yes" value="yes" />
-      <RadioButton id="passed-max-period-no" name="overdue_amounts" label="No" value="no" />
+      <RadioButton
+        id="passed-max-period-yes"
+        name="isPassedOverdueAmount"
+        label="Yes"
+        value="yes"
+        checked={isPassedOverdueAmount === 'yes'}
+        onChange={handleInputChange}
+      />
+      <RadioButton
+        id="passed-max-period-no"
+        name="isPassedOverdueAmount"
+        label="No"
+        value="no"
+        checked={isPassedOverdueAmount === 'no'}
+        onChange={handleInputChange}
+      />
 
       <div className="if-yes-row">
         <span className="font-primary mr-15">If yes, please provide details</span>
-        <Input type="text" placeholder="Details" />
+        <Input
+          type="text"
+          placeholder="Details"
+          name="passedOverdueDetails"
+          value={passedOverdueDetails}
+          onChange={handleInputChange}
+        />
       </div>
 
       <div className="f-14 font-secondary mb-10">
@@ -34,7 +98,13 @@ const ApplicationCreditLimitStep = () => {
 
       <div className="credit-limit-amount">
         <span className="font-primary mr-15">Amount*</span>
-        <Input type="text" placeholder="$00000" />
+        <Input
+          type="text"
+          name="creditLimit"
+          placeholder="$00000"
+          value={creditLimit}
+          onChange={handleInputChange}
+        />
       </div>
     </>
   );
