68a6830bb355611b6eea828387e17a30e8c77666
kanerodgers7 <jellyfish9629@gmail.com>
2021-04-07 17:44:23 +0530

    Confirmation step issue solved

--- src/screens/Application/GenerateApplication/component/ApplicationConfirmationStep/ApplicationConfirmationStep.js
+++ src/screens/Application/GenerateApplication/component/ApplicationConfirmationStep/ApplicationConfirmationStep.js
@@ -1,190 +1,204 @@
-import React, { useCallback } from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useCallback, useEffect } from 'react';
 import moment from 'moment';
 import './ApplicationConfirmationStep.scss';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Checkbox from '../../../../../common/Checkbox/Checkbox';
 import RadioButton from '../../../../../common/RadioButton/RadioButton';
+import { getApplicationDetail } from '../../../redux/ApplicationAction';
 
 const ApplicationConfirmationStep = () => {
-  const { company, creditLimit, partners } = useSelector(
+  const dispatch = useDispatch();
+  const editApplication = useSelector(({ application }) => application.editApplication);
+
+  const { company, creditLimit, partners, documents } = useSelector(
     ({ application }) => application.viewApplicationDetails
   );
+  console.log('viewApplication', company, creditLimit, partners, documents);
 
-  console.log('viewApplication', company, creditLimit, partners);
+  useEffect(() => {
+    console.log('dispatch happen');
+    if (editApplication.applicationId) {
+      console.log(21);
+      dispatch(getApplicationDetail(editApplication.applicationId));
+    }
+  }, [editApplication]);
 
   function getPersonStepData(personStep) {
-    return personStep.map(person => {
-      console.log('person', person);
-      if (person.type === 'individual') {
+    return (
+      personStep &&
+      personStep?.map(person => {
+        if (person.type === 'individual') {
+          return [
+            {
+              title: 'Individual Details',
+              type: 'title',
+            },
+            {
+              title: 'Title*',
+              value: person?.title || '-',
+              label: 'directorTitle',
+              type: 'select',
+            },
+            {
+              type: 'blank',
+            },
+            {
+              title: 'First Name *',
+              value: person?.firstName || '-',
+              label: 'directorFirstName',
+              type: 'text',
+            },
+            {
+              title: 'Middle Name',
+              value: person?.middleName || '-',
+              label: 'directorMiddleName',
+              type: 'text',
+            },
+            {
+              title: 'Last Name',
+              value: person?.lastName || '-',
+              label: 'directorLastName',
+              type: 'text',
+            },
+            {
+              title: 'Date of Birth *',
+              value: person.dateOfBirth ? moment(person.dateOfBirth).format('DD/MM/YYYY') : '-',
+              label: 'directorDOB',
+              type: 'text',
+            },
+            {
+              title:
+                'Do you give your consent for us to check your credit history with external credit agencies?',
+              value: person?.allowToCheckCreditHistory || false,
+              label: 'directorLastName',
+              type: 'checkbox',
+            },
+            {
+              title: 'Identification Details',
+              type: 'title',
+            },
+            {
+              title: 'Driver License Number',
+              value: person?.driverLicenceNumber || '-',
+              label: 'directorLicenseNumber',
+              type: 'text',
+            },
+            {
+              title: 'Unit Number',
+              value: person?.unitNumber || '-',
+              label: 'directorUnitNumber',
+              type: 'text',
+            },
+            {
+              title: 'Street Number *',
+              value: person?.streetNumber || '-',
+              label: 'directorStreetNumber',
+              type: 'text',
+            },
+            {
+              title: 'Street Name',
+              value: person?.streetName || '-',
+              label: 'directorStreetName',
+              type: 'text',
+            },
+            {
+              title: 'State *',
+              value: person?.state || '-',
+              label: 'directorState',
+              type: 'select',
+            },
+            {
+              title: 'Postcode *',
+              value: person?.postCode || '-',
+              label: 'directorPostCode',
+              type: 'text',
+            },
+            {
+              title: 'Contact Details',
+              type: 'title',
+            },
+            {
+              title: 'Phone Number',
+              value: person?.phoneNumber || '-',
+              label: 'directorPhoneNumber',
+              type: 'text',
+            },
+            {
+              title: 'Mobile',
+              value: person?.mobileNumber || '-',
+              label: 'directorMobile',
+              type: 'text',
+            },
+            {
+              title: 'Email Address',
+              value: person?.email || '-',
+              label: 'directorEmail',
+              type: 'text',
+            },
+            { type: 'line' },
+          ];
+        }
         return [
           {
-            title: 'Individual Details',
+            title: 'Company Details',
             type: 'title',
           },
           {
-            title: 'Title*',
-            value: person?.title || '-',
-            label: 'directorTitle',
+            title: 'Trading Name',
+            value: person?.tradingName || '-',
+            label: 'tradingName',
             type: 'text',
           },
           {
             type: 'blank',
           },
           {
-            title: 'First Name *',
-            value: person?.firstName || '-',
-            label: 'directorFirstName',
+            title: 'Entity Type*',
+            value: person?.entityType || '-',
+            label: 'entityType',
+            type: 'select',
+          },
+          {
+            title: 'Entity Name*',
+            value: person?.entityName || '-',
+            label: 'entityName',
+            type: 'select',
+          },
+          {
+            title: 'ACN',
+            value: person?.acn || '-',
+            label: 'acn',
             type: 'text',
           },
           {
-            title: 'Middle Name',
-            value: person?.middleName || '-',
-            label: 'directorMiddleName',
+            title: 'ABN*',
+            value: person?.abn || '-',
+            label: 'abn',
             type: 'text',
           },
-          {
-            title: 'Last Name',
-            value: person?.lastName || '-',
-            label: 'directorLastName',
-            type: 'text',
-          },
-          {
-            title: 'Date of Birth *',
-            value: person.dateOfBirth ? moment(person.dateOfBirth).format('DD/MM/YYYY') : '-',
-            label: 'directorDOB',
-            type: 'text',
-          },
-          {
-            title:
-              'Do you give your consent for us to check your credit history with external credit agencies?',
-            value: person?.allowToCheckCreditHistory || false,
-            label: 'directorLastName',
-            type: 'checkbox',
-          },
-          {
-            title: 'Identification Details',
-            type: 'title',
-          },
-          {
-            title: 'Driver License Number',
-            value: person?.driverLicenceNumber || '-',
-            label: 'directorLicenseNumber',
-            type: 'text',
-          },
-          {
-            title: 'Unit Number',
-            value: person?.unitNumber || '-',
-            label: 'directorUnitNumber',
-            type: 'text',
-          },
-          {
-            title: 'Street Number *',
-            value: person?.streetNumber || '-',
-            label: 'directorStreetNumber',
-            type: 'text',
-          },
-          {
-            title: 'Street Name',
-            value: person?.streetName || '-',
-            label: 'directorStreetName',
-            type: 'text',
-          },
-          {
-            title: 'State *',
-            value: person?.state || '-',
-            label: 'directorState',
-            type: 'text',
-          },
-          {
-            title: 'Postcode *',
-            value: person?.postCode || '-',
-            label: 'directorPostCode',
-            type: 'text',
-          },
-          {
-            title: 'Contact Details',
-            type: 'title',
-          },
-          {
-            title: 'Phone Number',
-            value: person?.phoneNumber || '-',
-            label: 'directorPhoneNumber',
-            type: 'text',
-          },
-          {
-            title: 'Mobile',
-            value: person?.mobileNumber || '-',
-            label: 'directorMobile',
-            type: 'text',
-          },
-          {
-            title: 'Email Address',
-            value: person?.email || '-',
-            label: 'directorEmail',
-            type: 'text',
-          },
+
           { type: 'line' },
         ];
-      }
-      return [
-        {
-          title: 'Company Details',
-          type: 'title',
-        },
-        {
-          title: 'Trading Name',
-          value: person?.tradingName || '-',
-          label: 'tradingName',
-          type: 'text',
-        },
-        {
-          type: 'blank',
-        },
-        {
-          title: 'Entity Type*',
-          value: person?.entityType || '-',
-          label: 'entityType',
-          type: 'text',
-        },
-        {
-          title: 'Entity Name*',
-          value: person?.entityName || '-',
-          label: 'entityName',
-          type: 'text',
-        },
-        {
-          title: 'ACN',
-          value: person?.acn || '-',
-          label: 'acn',
-          type: 'text',
-        },
-        {
-          title: 'ABN*',
-          value: person?.abn || '-',
-          label: 'abn',
-          type: 'text',
-        },
-
-        { type: 'line' },
-      ];
-    });
+      })
+    );
   }
 
   const confirmationDetails = [
     {
       title: 'Client Name',
-      value: company?.clientId.name || '-',
+      value: company?.clientId || '-',
       label: 'clientName',
-      type: 'text',
+      type: 'select',
     },
     {
       type: 'line',
     },
     {
       title: 'Debtor',
-      value: company?.debtorId.name || '-',
+      value: company?.debtorId || '-',
       label: 'debtor',
-      type: 'text',
+      type: 'select',
     },
     {
       title: 'Trading Name',
@@ -208,7 +222,7 @@ const ApplicationConfirmationStep = () => {
       title: 'Entity Name*',
       value: company?.entityName || '-',
       label: 'entityName',
-      type: 'text',
+      type: 'select',
     },
     {
       title: 'Phone Number',
@@ -220,7 +234,7 @@ const ApplicationConfirmationStep = () => {
       title: 'Entity Type*',
       value: company?.entityType || '-',
       label: 'entityType',
-      type: 'text',
+      type: 'select',
     },
     {
       title: 'Outstanding Amount',
@@ -263,7 +277,7 @@ const ApplicationConfirmationStep = () => {
       title: 'State',
       value: company?.state || '-',
       label: 'state',
-      type: 'text',
+      type: 'select',
     },
     {
       title: 'Postcode',
@@ -275,13 +289,13 @@ const ApplicationConfirmationStep = () => {
       title: 'Street Type',
       value: company?.streetType || '-',
       label: 'streetType',
-      type: 'text',
+      type: 'select',
     },
     {
       title: 'Country',
-      value: company?.country.name || '-',
+      value: company?.country || '-',
       label: 'country',
-      type: 'text',
+      type: 'select',
     },
     {
       title: 'Suburb',
@@ -337,83 +351,92 @@ const ApplicationConfirmationStep = () => {
     },
     {
       type: 'array',
-      data: getPersonStepData(partners),
+      data: getPersonStepData(partners || []),
     },
   ];
-  const getConfirmationComponentFromType = useCallback(detail => {
-    switch (detail.type) {
-      case 'text':
-        return (
-          <>
-            <span>{detail.title}</span>
-            <span className="detail-value">{detail.value}</span>
-          </>
-        );
-      case 'ifYesText':
-        return (
-          <>
-            <span>{detail.title}</span>
-            <span className="long-text">{detail.value}</span>
-          </>
-        );
-      case 'title':
-        return (
-          <>
-            <span className="title">{detail.title}</span>
-          </>
-        );
-      case 'radio':
-        return (
-          <>
-            <span className="radio-title">{detail.title}</span>
-            {console.log(detail.label, detail.value)}
-            <span className="radio-buttons">
-              <RadioButton
-                disabled
-                id={`${detail.id}-yes`}
-                name={detail.name}
-                label="Yes"
-                value
-                checked={detail.value}
-              />
-              <RadioButton
-                disabled
-                id={`${detail.id}-no`}
-                name={detail.name}
-                label="No"
-                value={false}
-                checked={!detail.value}
-              />
-            </span>
-          </>
-        );
-      case 'checkbox':
-        return (
-          <>
-            <Checkbox className="grid-checkbox" title={detail.title} />
-          </>
-        );
-      case 'main-title':
-        return (
-          <>
-            <div className="main-title">{detail.title}</div>
-          </>
-        );
-      case 'line':
-        return <div className="horizontal-line" />;
-      case 'blank':
-        return (
-          <>
-            <div />
-            <div />
-          </>
-        );
-      case 'array':
-        return detail.data.map(elem => elem.map(getConfirmationComponentFromType));
-      default:
-        return null;
-    }
-  }, []);
+  const getConfirmationComponentFromType = useCallback(
+    detail => {
+      switch (detail.type) {
+        case 'text':
+          return (
+            <>
+              <span>{detail?.title}</span>
+              <span className="detail-value">{detail?.value}</span>
+            </>
+          );
+        case 'select':
+          return (
+            <>
+              <span>{detail?.title}</span>
+              <span className="detail-value">{detail?.value[0]?.label}</span>
+            </>
+          );
+        case 'ifYesText':
+          return (
+            <>
+              <span>{detail.title}</span>
+              <span className="long-text">{detail.value}</span>
+            </>
+          );
+        case 'title':
+          return (
+            <>
+              <span className="title">{detail.title}</span>
+            </>
+          );
+        case 'radio':
+          return (
+            <>
+              <span className="radio-title">{detail.title}</span>
+              <span className="radio-buttons">
+                <RadioButton
+                  disabled
+                  id={`${detail.id}-yes`}
+                  name={detail.name}
+                  label="Yes"
+                  value
+                  checked={detail.value}
+                />
+                <RadioButton
+                  disabled
+                  id={`${detail.id}-no`}
+                  name={detail.name}
+                  label="No"
+                  value={false}
+                  checked={!detail.value}
+                />
+              </span>
+            </>
+          );
+        case 'checkbox':
+          return (
+            <>
+              <Checkbox className="grid-checkbox" title={detail.title} />
+            </>
+          );
+        case 'main-title':
+          return (
+            <>
+              <div className="main-title">{detail.title}</div>
+            </>
+          );
+        case 'line':
+          return <div className="horizontal-line" />;
+        case 'blank':
+          return (
+            <>
+              <div />
+              <div />
+            </>
+          );
+        case 'array':
+          return detail.data.map(elem => elem.map(getConfirmationComponentFromType));
+        default:
+          return null;
+      }
+    },
+    [partners]
+  );
   return (
     <div className="application-confirmation-step">
       {confirmationDetails.map(getConfirmationComponentFromType)}
