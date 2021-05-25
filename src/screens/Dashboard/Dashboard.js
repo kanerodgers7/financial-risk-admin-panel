f62dbd732470ae72e5ad9ef8f801ae8ca743fa51
kanerodgers7 <jellyfish9629@gmail.com>
2021-05-25 09:48:26 +0530

    Squashed commit: Merged feature/dashboard-monica to development
    commit 847c104dfb23ca2785678fb1f27b0d348735fbea
    Author: Monika Parmar <monika.parmar@kevit.io>
    Date:   Thu May 20 19:53:26 2021 +0530
    
        dashboard design done
    
    commit 721930ce71a25153f5bf85e0eca7a6d23fe4dd93
    Author: Monika Parmar <monika.parmar@kevit.io>
    Date:   Thu May 20 19:53:11 2021 +0530
    
        react-select__indicators class align-self center given
    
    commit 86cff3c1ed698a3ed5fc279cff841b6f7d268810
    Author: Monika Parmar <monika.parmar@kevit.io>
    Date:   Thu May 20 19:52:53 2021 +0530
    
        dashboard css created

--- src/screens/Dashboard/Dashboard.js
+++ src/screens/Dashboard/Dashboard.js
@@ -1,8 +1,26 @@
+import ReactSelect from "react-select";
+
 const Dashboard = () => {
   return (
-    <div className="page-header">
-      <div className="page-header-name">Dashboard</div>
-    </div>
+<>
+      <ReactSelect isMulti className="react-select-container dashboard-select-container"
+                    classNamePrefix="react-select"
+                   placeholder='Select person'/>
+        <div className="dashboard-grid">
+            <div className="dashboard-white-container">
+                <div className="dashboard-white-container-icon"><span className="material-icons-round">assignment</span> </div>
+                <div className="title">Pending Application</div>
+                <div className="readings">51</div>
+                <div className="dashboard-white-container-stripe"/>
+            </div>
+            <div className="dashboard-white-container">
+                <div className="dashboard-white-container-icon"><span className="material-icons-round">task</span> </div>
+                <div className="title">Pending Tasks</div>
+                <div className="readings">22</div>
+                <div className="dashboard-white-container-stripe"/>
+            </div>
+        </div>
+    </>
   );
 };
 export default Dashboard;
