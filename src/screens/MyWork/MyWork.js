5b19adafc35540714fdc00d4b8bde8faa73e1d3e
kanerodgers7 <jellyfish9629@gmail.com>
2021-09-09 17:56:48 +0530

    task - condition set as per user module access

--- src/screens/MyWork/MyWork.js
+++ src/screens/MyWork/MyWork.js
@@ -1,17 +1,25 @@
-import React, { useCallback, useState } from 'react';
-
+import React, { useCallback, useMemo, useState } from 'react';
 import Tab from '../../common/Tab/Tab';
 import MyWorkTasks from './MyWorkTasks/MyWorkTasks';
 
 import MyWorkNotifications from './MyWorkNotifications/MyWorkNotifications';
+import { useModulePrivileges } from '../../hooks/userPrivileges/useModulePrivilegesHook';
+import Loader from '../../common/Loader/Loader';
 
 const MyWork = () => {
-  const myWorkTabs = ['Tasks', 'Notifications'];
+  const access = useModulePrivileges('task');
 
   const [activeTabIndex, setActiveTabIndex] = useState(0);
 
-  const myWorkTabContent = [<MyWorkTasks />, <MyWorkNotifications />];
-
+  const myWorkTabs = useMemo(
+    () => (access.hasReadAccess ? ['Tasks', 'Notifications'] : ['Notifications']),
+    [access]
+  );
+  const myWorkTabContent = useMemo(
+    () =>
+      access.hasReadAccess ? [<MyWorkTasks />, <MyWorkNotifications />] : [<MyWorkNotifications />],
+    [access]
+  );
   const tabActive = useCallback(
     index => {
       setActiveTabIndex(index);
@@ -21,15 +29,21 @@ const MyWork = () => {
 
   return (
     <>
-      <div className="my-work-tab-button-row">
-        <Tab
-          tabs={myWorkTabs}
-          tabActive={tabActive}
-          activeTabIndex={activeTabIndex}
-          className="my-work-tab"
-        />
-      </div>
-      <div className="my-work-tab-content-container">{myWorkTabContent[activeTabIndex]}</div>
+      {myWorkTabs.length > 0 ? (
+        <>
+          <div className="my-work-tab-button-row">
+            <Tab
+              tabs={myWorkTabs}
+              tabActive={tabActive}
+              activeTabIndex={activeTabIndex}
+              className="my-work-tab"
+            />
+          </div>
+          <div className="my-work-tab-content-container">{myWorkTabContent[activeTabIndex]}</div>{' '}
+        </>
+      ) : (
+        <Loader />
+      )}
     </>
   );
 };
