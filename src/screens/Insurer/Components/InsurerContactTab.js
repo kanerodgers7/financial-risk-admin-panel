cdc3913013135caafd715067a3ea9f6dbccd57f9
kanerodgers7 <jellyfish9629@gmail.com>
2021-09-29 12:38:38 +0530

    https://app.clickup.com/t/3409307/DEV-15002 - Fix

--- src/screens/Insurer/Components/InsurerContactTab.js
+++ src/screens/Insurer/Components/InsurerContactTab.js
@@ -206,28 +206,33 @@ const InsurerContactTab = () => {
           )}
         </div>
       </div>
+      {/* eslint-disable-next-line no-nested-ternary */}
       {docs ? (
-        <>
-          <div className="tab-table-container">
-            <Table
-              align="left"
-              valign="center"
-              tableClass="white-header-table"
-              data={docs}
-              headers={headers}
-              refreshData={getInsurerContactsList}
+        docs.length > 0 ? (
+          <>
+            <div className="tab-table-container">
+              <Table
+                align="left"
+                valign="center"
+                tableClass="white-header-table"
+                data={docs}
+                headers={headers}
+                refreshData={getInsurerContactsList}
+              />
+            </div>
+            <Pagination
+              className="common-list-pagination"
+              total={total}
+              pages={pages}
+              page={page}
+              limit={limit}
+              pageActionClick={pageActionClick}
+              onSelectLimit={onSelectLimit}
             />
-          </div>
-          <Pagination
-            className="common-list-pagination"
-            total={total}
-            pages={pages}
-            page={page}
-            limit={limit}
-            pageActionClick={pageActionClick}
-            onSelectLimit={onSelectLimit}
-          />
-        </>
+          </>
+        ) : (
+          <div className="no-record-found">No record found</div>
+        )
       ) : (
         <Loader />
       )}
