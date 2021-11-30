59673782456c89871d28a12feec57d56849aa78d
kanerodgers7 <jellyfish9629@gmail.com>
2021-11-30 11:51:01 +0530

    on click close import application procedure, while calling getApplicationListByFilter - filters sent in request

--- src/screens/Application/ImportApplication/ImportApplicationModal.js
+++ src/screens/Application/ImportApplication/ImportApplicationModal.js
@@ -8,20 +8,64 @@ import {
   getApplicationsListByFilter,
   resetImportApplicationStepper,
 } from '../redux/ApplicationAction';
+import { useQueryParams } from '../../../hooks/GetQueryParamHook';
 
 const ImportApplicationModal = () => {
   const dispatch = useDispatch();
   const { importApplication } = useSelector(({ application }) => application ?? {});
+  const { applicationListFilters } = useSelector(
+    ({ listFilterReducer }) => listFilterReducer ?? {}
+  );
   const activeStep = useMemo(() => importApplication?.activeStep ?? 0, [importApplication]);
   const importId = useMemo(() => importApplication?.importId ?? null, [importApplication]);
+
+  const {
+    page: paramPage,
+    limit: paramLimit,
+    entityType: paramEntityType,
+    status: paramStatus,
+    minCreditLimit: paramMinCreditLimit,
+    maxCreditLimit: paramMaxCreditLimit,
+    startDate: paramStartDate,
+    endDate: paramEndDate,
+  } = useQueryParams();
 
   const [importApplicationModal, setImportApplicationModal] = useState(false);
   const toggleImportApplicationModal = useCallback(() => {
     setImportApplicationModal(e => !e);
   }, []);
   const oncancelImportApplicationModal = useCallback(() => {
+    const params = {
+      page: paramPage ?? 1,
+      limit: paramLimit ?? 15,
+    };
+    const filters = {
+      entityType:
+        (paramEntityType?.trim()?.length ?? -1) > 0
+          ? paramEntityType
+          : applicationListFilters?.entityType ?? undefined,
+      clientId: applicationListFilters?.clientId,
+      debtorId: applicationListFilters?.debtorId,
+
+      status:
+        (paramStatus?.trim()?.length ?? -1) > 0
+          ? paramStatus
+          : applicationListFilters?.status,
+      minCreditLimit:
+        (paramMinCreditLimit?.trim()?.length ?? -1) > 0
+          ? paramMinCreditLimit
+          : applicationListFilters?.minCreditLimit,
+      maxCreditLimit:
+        (paramMaxCreditLimit?.trim()?.length ?? -1) > 0
+          ? paramMaxCreditLimit
+          : applicationListFilters?.maxCreditLimit,
+      startDate: paramStartDate || applicationListFilters?.startDate,
+      endDate: paramEndDate || applicationListFilters?.endDate,
+    };
+
     if (activeStep >= 2 && activeStep <= 4) dispatch(deleteDumpFromBackend(importId));
-    if (activeStep === 4) dispatch(getApplicationsListByFilter());
+
+    if (activeStep === 4) dispatch(getApplicationsListByFilter({...params, ...filters}));
     dispatch(resetImportApplicationStepper());
     toggleImportApplicationModal();
   }, [activeStep, importId]);
