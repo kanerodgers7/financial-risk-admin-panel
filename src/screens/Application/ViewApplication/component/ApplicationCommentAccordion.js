ec83681b23e817e43c8f4594ce1c984c457df15e
kanerodgers7 <jellyfish9629@gmail.com>
2023-04-07 00:32:53 -0700

    added Edit button on debtor comment for Application detail page.

--- src/screens/Application/ViewApplication/component/ApplicationCommentAccordion.js
+++ src/screens/Application/ViewApplication/component/ApplicationCommentAccordion.js
@@ -41,28 +41,34 @@ const ApplicationCommentAccordion = props => {
     <>
       <AccordionItem index={index} header="Comment" suffix="expand_more">
         <div className="common-accordion-item-content-box">
-          {status?.value === 'DECLINED' || status?.value === 'APPROVED' ? (
+          {/* {status?.value === 'DECLINED' || status?.value === 'APPROVED' ? (
             <textarea
               className="mt-5 w-100"
               style={{ border: 'none' }}
               disabled
               value={comments ? `${comments}` : '-'}
             />
-          ) : (
-            <>
-              <textarea
-                rows={3}
-                name="comment"
-                className="mt-5 w-100"
-                placeholder="Enter Comment"
-                value={commentText}
-                onChange={e => setCommentText(e?.target?.value)}
-              />
-              <div className="d-flex just-end align-center mt-5">
-                <Button buttonType="primary" className="small-button" title="save" onClick={saveClientComment} />
-              </div>
-            </>
-          )}
+          ) : ( */}
+
+          <>
+            <textarea
+              rows={3}
+              name="comment"
+              className="mt-5 w-100"
+              placeholder="Enter Comment"
+              value={commentText}
+              onChange={e => setCommentText(e?.target?.value)}
+            />
+            <div className="d-flex just-end align-center mt-5">
+              {status?.value === 'DECLINED' || status?.value === 'APPROVED' ? (
+                <Button buttonType="primary" className="small-button" title="Edit" onClick={saveClientComment} />
+              ) : (
+                <Button buttonType="primary" className="small-button" title="Save" onClick={saveClientComment} />
+              )}
+            </div>
+          </>
+          
+          {/* )} */}
         </div>
       </AccordionItem>
     </>
